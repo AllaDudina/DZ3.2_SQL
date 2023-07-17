c7fa2d46d9b63440060b7d92b6b8ba2bf9d36fa3
Alla <thedreamtea@gmail.com>
2023-07-17 20:38:37 +1200

    Initial commit

---
+++ src/test/java/test/AppTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import data.DataHelper;
+import data.SqlRequest;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @AfterAll
+    static void clear() {
+        SqlRequest.clearBD();
+    }
+
+    @Test
+    void shouldRunAppWithValidData() {
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = SqlRequest.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        dashboardPage.validFields();
+    }
+
+    @Test
+    void shouldWarnIfRunAppWithInvalidLogin() {
+        val loginPage = new LoginPage();
+        val invalidAuthInfo = DataHelper.getInvalidAuthInfo();
+        loginPage.invalidLogin(invalidAuthInfo);
+    }
+}
